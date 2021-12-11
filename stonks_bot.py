d9ac766ebdc3f1494a40fdceb1aea89b49b90185
carolis <65249499+carolina-ml@users.noreply.github.com>
2021-12-10 22:48:12 -0300

    extraindo cada coluna do rico

--- stonks_bot.py
+++ stonks_bot.py
@@ -1,6 +1,10 @@
 import csv
 
+lista_de_acoes = []
+
 with open('stonks_bot.csv', 'r') as stonks:
-    tabela = csv.reader(stonks)
-    for vinho in tabela:    
-        print(vinho)
+    tabela = csv.DictReader(stonks)
+    for linha in tabela:
+        if linha['INVESTIMENTOS']=='RICO':    
+            lista_de_acoes.append(linha['renda variável'])
+print(lista_de_acoes)            
